5b8bd246ab1180b2a187b364e10d722f4f74bd74
supernova <76390419+driedpampas@users.noreply.github.com>
2024-12-09 23:21:34 +0200

    Coins and a coin counter

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_mixer.h>
+#include <SDL2/SDL_ttf.h>
 #include <iostream>
 #include <vector>
 #include <string>
@@ -19,35 +20,22 @@ struct GameObject {
     SDL_Rect rect;
 };
 
-std::vector<int> checkIntersections(const SDL_Rect* A, const SDL_Rect* B) {
-    std::vector<int> intersections;
-
+bool hasIntersection(const SDL_Rect* A, const SDL_Rect* B) {
     if (!A || !B) {
-        return intersections;
+        return false;
     }
 
-    if (A->x + A->w > B->x && A->x < B->x + B->w) {
-        if (A->y + A->h <= B->y) {
-            intersections.push_back(1); // Intersection above
-        } else if (A->y >= B->y + B->h) {
-            intersections.push_back(3); // Intersection below
+    if (A->x + A->w <= B->x || B->x + B->w <= A->x ||
+        A->y + A->h <= B->y || B->y + B->h <= A->y) {
+        return false;
         }
-    }
 
-    if (A->y + A->h > B->y && A->y < B->y + B->h) {
-        if (A->x + A->w <= B->x) {
-            intersections.push_back(2); // Intersection right
-        } else if (A->x >= B->x + B->w) {
-            intersections.push_back(4); // Intersection left
-        }
-    }
-
-    if (intersections.empty()) {
-        intersections.push_back(0); // No intersection
-    }
-
-    return intersections;
+    return true;
 }
+
+int totalCoins = 0;
+int collectedCoins = 0;
+TTF_Font* font = nullptr;
 
 bool init(SDL_Window*& window, SDL_Renderer*& renderer) {
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0) {
@@ -67,7 +55,7 @@ bool init(SDL_Window*& window, SDL_Renderer*& renderer) {
         return false;
     }
 
-    if (constexpr int imgFlags = IMG_INIT_PNG; !(IMG_Init(imgFlags) & imgFlags)) {
+    if (int imgFlags = IMG_INIT_PNG; !(IMG_Init(imgFlags) & imgFlags)) {
         cerr << "SDL_image could not initialize! SDL_image Error: " << IMG_GetError() << endl;
         return false;
     }
@@ -77,21 +65,27 @@ bool init(SDL_Window*& window, SDL_Renderer*& renderer) {
         return false;
     }
 
+    if (TTF_Init() == -1) {
+        cerr << "SDL_ttf could not initialize! SDL_ttf Error: " << TTF_GetError() << endl;
+        return false;
+    }
+
+    font = TTF_OpenFont("../resources/opensans.ttf", 24);
+    if (!font) {
+        cerr << "Failed to load font! SDL_ttf Error: " << TTF_GetError() << endl;
+        return false;
+    }
+
     return true;
 }
 
-void loadLevel(const string& filePath, vector<GameObject>& gameObjects, SDL_Renderer* renderer, SDL_Texture* brickTexture, SDL_Texture* vineTexture, SDL_Texture* marioTexture, SDL_Texture* starCoinTexture ,GameObject& player) {
+void loadLevel(const string& filePath, vector<GameObject>& gameObjects, SDL_Renderer* renderer, SDL_Texture* brickTexture, SDL_Texture* vineTexture, SDL_Texture* marioTexture, SDL_Texture* starCoinTexture, GameObject& player) {
     ifstream levelFile(filePath);
     string line;
     int y = 0;
     int playerInit = 0;
 
     while (getline(levelFile, line)) {
-        size_t commentPos = line.find('#');
-        if (commentPos != string::npos) {
-            line = line.substr(0, commentPos);
-        }
-
         for (int x = 0; x < line.length(); ++x) {
             char tile = line[x];
             SDL_Rect rect = { x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE };
@@ -101,6 +95,7 @@ void loadLevel(const string& filePath, vector<GameObject>& gameObjects, SDL_Rend
                 gameObjects.push_back({ vineTexture, rect });
             } else if (tile == '+') {
                 gameObjects.push_back({ starCoinTexture, rect });
+                totalCoins++;
             } else if (tile == '@') {
                 if (++playerInit > 1) {
                     throw runtime_error("Error: Player character initialized more than once!");
@@ -110,6 +105,16 @@ void loadLevel(const string& filePath, vector<GameObject>& gameObjects, SDL_Rend
         }
         ++y;
     }
+}
+
+void renderText(SDL_Renderer* renderer, const string& text, int x, int y) {
+    SDL_Color textColor = { 255, 255, 255, 255 };
+    SDL_Surface* textSurface = TTF_RenderText_Solid(font, text.c_str(), textColor);
+    SDL_Texture* textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
+    SDL_Rect renderQuad = { x, y, textSurface->w, textSurface->h };
+    SDL_RenderCopy(renderer, textTexture, nullptr, &renderQuad);
+    SDL_FreeSurface(textSurface);
+    SDL_DestroyTexture(textTexture);
 }
 
 bool hasIntersection(const SDL_Rect& a, const SDL_Rect& b) {
@@ -139,59 +144,19 @@ bool isOnPlatform(const GameObject& player, const vector<GameObject>& gameObject
     return false;
 }
 
-void handleInput(const SDL_Event& e, GameObject& player, const vector<GameObject>& gameObjects, SDL_Texture* vineTexture, SDL_Texture* marioTextureLeft, SDL_Texture* marioTextureRight, bool& quit, bool& musicPlaying, Mix_Music* soundtrack) {
-    if (e.type == SDL_KEYDOWN) {
-        SDL_Rect newRect = player.rect;
-        constexpr int moveSpeed = TILE_SIZE;
+enum GameState {
+    PLAYING,
+    WON
+};
 
-        switch (e.key.keysym.sym) {
-        case SDLK_w:
-            newRect.y -= moveSpeed;
-            break;
-        case SDLK_s:
-            newRect.y += moveSpeed;
-            break;
-        case SDLK_a:
-            newRect.x -= moveSpeed;
-            player.texture = marioTextureLeft;
-            break;
-        case SDLK_d:
-            newRect.x += moveSpeed;
-            player.texture = marioTextureRight;
-            break;
-        case SDLK_m:
-            if (musicPlaying) {
-                Mix_PauseMusic();
-            } else {
-                Mix_ResumeMusic();
-            }
-            musicPlaying = !musicPlaying;
-            break;
-        case SDLK_ESCAPE:
-            quit = true;
-            break;
-        default: break;
-        }
+void renderWinningScreen(SDL_Renderer* renderer) {
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
 
-        // Ensure the player does not go out of the screen's bounds
-        if (newRect.x < 0) newRect.x = 0;
-        if (newRect.x + newRect.w > SCREEN_WIDTH) newRect.x = SCREEN_WIDTH - newRect.w;
-        if (newRect.y < 0) newRect.y = 0;
-        if (newRect.y + newRect.h > SCREEN_HEIGHT) newRect.y = SCREEN_HEIGHT - newRect.h;
+    renderText(renderer, "You Won!", SCREEN_WIDTH / 2 - 50, SCREEN_HEIGHT / 2 - 50);
+    renderText(renderer, "Press Space to end the game", SCREEN_WIDTH / 2 - 150, SCREEN_HEIGHT / 2);
 
-        bool collision = false;
-        for (const auto& obj : gameObjects) {
-            if (obj.texture == player.texture) continue; // Skip player texture
-            if (obj.texture != vineTexture && hasIntersection(newRect, obj.rect)) {
-                collision = true;
-                break;
-            }
-        }
-
-        if (!collision) {
-            player.rect = newRect;
-        }
-    }
+    SDL_RenderPresent(renderer);
 }
 
 int main(int argc, char* args[]) {
@@ -233,51 +198,131 @@ int main(int argc, char* args[]) {
 
     bool quit = false;
     SDL_Event e;
+    GameState gameState = PLAYING;
 
     while (!quit) {
         while (SDL_PollEvent(&e) != 0) {
             if (e.type == SDL_QUIT) {
                 quit = true;
             }
-            handleInput(e, player, gameObjects, vineTexture, marioTextureLeft, marioTextureRight, quit, musicPlaying, soundtrack);
+            if (e.type == SDL_KEYDOWN) {
+                if (gameState == WON && e.key.keysym.sym == SDLK_SPACE) {
+                    quit = true;
+                } else if (gameState == PLAYING) {
+                    SDL_Rect newRect = player.rect;
+                    constexpr int moveSpeed = TILE_SIZE;
+
+                    switch (e.key.keysym.sym) {
+                    case SDLK_w:
+                        newRect.y -= moveSpeed;
+                        break;
+                    case SDLK_s:
+                        newRect.y += moveSpeed;
+                        break;
+                    case SDLK_a:
+                        newRect.x -= moveSpeed;
+                        player.texture = marioTextureLeft;
+                        break;
+                    case SDLK_d:
+                        newRect.x += moveSpeed;
+                        player.texture = marioTextureRight;
+                        break;
+                    case SDLK_m:
+                        if (musicPlaying) {
+                            Mix_PauseMusic();
+                        } else {
+                            Mix_ResumeMusic();
+                        }
+                        musicPlaying = !musicPlaying;
+                        break;
+                    case SDLK_ESCAPE:
+                        quit = true;
+                        break;
+                    default: break;
+                    }
+
+                    // Ensure the player does not go out of the screen's bounds
+                    if (newRect.x < 0) newRect.x = 0;
+                    if (newRect.x + newRect.w > SCREEN_WIDTH) newRect.x = SCREEN_WIDTH - newRect.w;
+                    if (newRect.y < 0) newRect.y = 0;
+                    if (newRect.y + newRect.h > SCREEN_HEIGHT) newRect.y = SCREEN_HEIGHT - newRect.h;
+
+                    bool collision = false;
+
+                    // Check for collisions with star coins
+                    for (auto it = gameObjects.begin(); it != gameObjects.end(); ++it) {
+                        if (it->texture == starCoinTexture && hasIntersection(newRect, it->rect)) {
+                            collectedCoins++;
+                            gameObjects.erase(it);
+                            break;
+                        }
+                    }
+
+                    // Check for collisions with other game objects
+                    for (const auto& obj : gameObjects) {
+                        if (obj.texture != vineTexture && obj.texture != starCoinTexture && hasIntersection(newRect, obj.rect)) {
+                            collision = true;
+                            break;
+                        }
+                    }
+
+                    if (!collision) {
+                        player.rect = newRect;
+                    }
+                }
+            }
         }
 
-        if (!isOnPlatform(player, gameObjects, brickTexture) && !isOnVine(player, gameObjects, vineTexture)) {
-            bool atTopOfVine = false;
-            SDL_Rect belowPlayer = player.rect;
-            belowPlayer.y += 1;
+        if (gameState == PLAYING) {
+            if (!isOnPlatform(player, gameObjects, brickTexture) && !isOnVine(player, gameObjects, vineTexture)) {
+                bool atTopOfVine = false;
+                SDL_Rect belowPlayer = player.rect;
+                belowPlayer.y += 1;
 
-            for (const GameObject& obj : gameObjects) {
-                if (obj.texture == vineTexture && hasIntersection(belowPlayer, obj.rect)) {
-                    atTopOfVine = true;
-                    break;
+                for (const auto& obj : gameObjects) {
+                    if (obj.texture == vineTexture && hasIntersection(belowPlayer, obj.rect)) {
+                        atTopOfVine = true;
+                        break;
+                    }
+                }
+
+                if (!atTopOfVine) {
+                    int gravity = 1;
+                    player.rect.y += gravity;
+                    if (player.rect.y + player.rect.h > SCREEN_HEIGHT) { // imagine falling off the screen :')
+                        player.rect.y = SCREEN_HEIGHT - player.rect.h;
+                    }
                 }
             }
 
-            if (!atTopOfVine) {
-                constexpr int gravity = 1;
-                player.rect.y += gravity;
-                if (player.rect.y + player.rect.h > SCREEN_HEIGHT) { // imagine falling off the screen :')
-                    player.rect.y = SCREEN_HEIGHT - player.rect.h;
-                }
+            SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
+            SDL_RenderClear(renderer);
+
+            SDL_RenderCopy(renderer, backgroundTexture, nullptr, nullptr);
+            for (const auto& obj : gameObjects) {
+                SDL_RenderCopy(renderer, obj.texture, nullptr, &obj.rect);
             }
+            SDL_RenderCopy(renderer, player.texture, nullptr, &player.rect);
+
+            // Render the coin counter
+            string coinText = "Coins: " + to_string(collectedCoins) + "/" + to_string(totalCoins);
+            renderText(renderer, coinText, 10, 10);
+
+            SDL_RenderPresent(renderer);
+
+            if (collectedCoins == totalCoins) {
+                gameState = WON;
+            }
+        } else {
+            renderWinningScreen(renderer);
         }
-
-        SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
-        SDL_RenderClear(renderer);
-
-        SDL_RenderCopy(renderer, backgroundTexture, nullptr, nullptr);
-        for (const GameObject& obj : gameObjects) {
-            SDL_RenderCopy(renderer, obj.texture, nullptr, &obj.rect);
-        }
-        SDL_RenderCopy(renderer, player.texture, nullptr, &player.rect);
-
-        SDL_RenderPresent(renderer);
     }
 
     Mix_FreeMusic(soundtrack);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
+    TTF_CloseFont(font);
+    TTF_Quit();
     Mix_Quit();
     IMG_Quit();
     SDL_Quit();
